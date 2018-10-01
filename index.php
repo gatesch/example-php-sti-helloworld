05622511a2dc0cf8080cc6d6eb85646aa9ffc3d1
root <root@exam1.example.com>
2018-10-01 11:54:58 +1000

    test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>HPE PHP Demo</title>
+    <title>Alex PHP Demo</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
